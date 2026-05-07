f45b9cb63fc2ec7255805dd6f0193b9e603df6f0
Arnav Parashetti <arnav.parashetti@gmail.com>
2026-05-06 23:23:05 -0400

    Refactor app.py for improved readability

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from stockfish import Stockfish
 
-engine = Stockfish()  # no path needed
+engine = Stockfish()  # no path neededk
 app = Flask(__name__)
 CORS(app)
 
